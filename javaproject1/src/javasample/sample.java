363d9cd37d1d003d0e0ba94d70f48160e6fa4886
bharathkrishna7 <bharathkrishna7777@gmail.com>
2019-06-11 13:11:04 +0530

    sample

--- javaproject1/src/javasample/sample.java
+++ javaproject1/src/javasample/sample.java
@@ -12,9 +12,19 @@ public class sample
 				System.out.println("print "+s);
 				System.out.println("print "+ch);
 			}
-
 			
 		}
+		
+		int a[]={24,64,84,65,41};
+		int big=a[0];
+		for(int i=1;i<=a.length-1;i++)
+		{
+			if(a[i]>big)
+			{
+				big=a[i];
+			}
+		}
+		System.out.println(big);
 	}
 
 }
